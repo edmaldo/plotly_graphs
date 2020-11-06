d0bcf84c8a55e4efa6df4a4cdd1c6faaf8c2bbcd
ed_maldo <ed_maldo@xxx.com>
2020-11-05 22:06:42 -0500

    earthquake graph

---
+++ earthquake_plot.py
@@ -0,0 +1,40 @@
+import json
+
+from plotly.graph_objs import Layout
+from plotly import offline
+
+filename = 'data/eq_data_30_day_m1.json'
+with open(filename) as f:
+    all_eq_data = json.load(f)
+
+all_eq_dicts = all_eq_data['features']
+
+mags, lons, lats, hover_texts = [], [], [], []
+for eq_dicts in all_eq_dicts:
+    mag = eq_dicts['properties']['mag']
+    lon = eq_dicts['geometry']['coordinates'][0]
+    lat = eq_dicts['geometry']['coordinates'][1]
+    title = eq_dicts['properties']['title']
+    mags.append(mag)
+    lons.append(lon)
+    lats.append(lat)
+    hover_texts.append(title)
+
+data = [{
+    'type': 'scattergeo',
+    'lon': lons,
+    'lat': lats,
+    'text': hover_texts,
+    'marker': {
+        'size': [5*mag for mag in mags],
+        'color': mags,
+        'colorscale': 'Jet',
+        'reversescale': True,
+        'colorbar': {'title': 'Magnitude'}
+    },
+        }]
+
+my_layout = Layout(title='Global Earthquakes')
+
+fig = {'data': data, 'layout': my_layout}
+offline.plot(fig, filename='earthquake_world_map.html')
